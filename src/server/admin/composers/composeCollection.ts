e73447b03f6ced4b5cfde49108000fc87e46d81f
ndvo <nelson@ocastudios.com>
2021-06-15 18:17:46 -0300

    chore: add foxy.io eslint config dependency

--- src/server/admin/composers/composeCollection.ts
+++ src/server/admin/composers/composeCollection.ts
@@ -9,13 +9,15 @@ type Params = {
   composeItem: (item: any) => any;
 };
 
-export function composeCollection({
-  url,
-  rel,
-  count,
-  items,
-  composeItem,
-}: Params): HALJSONResource {
+/**
+ * @param root0
+ * @param root0.url
+ * @param root0.rel
+ * @param root0.count
+ * @param root0.items
+ * @param root0.composeItem
+ */
+export function composeCollection({ url, rel, count, items, composeItem }: Params) {
   const limitInQuery = parseInt(new URL(url).searchParams.get('limit') ?? '20');
   const limit = isNaN(limitInQuery) || limitInQuery > 300 || limitInQuery < 0 ? 20 : limitInQuery;
 
