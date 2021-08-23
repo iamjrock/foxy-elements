be9b67059be7d84f36259c490b20cf63d21dd36f
ndvo <nelson@ocastudios.com>
2021-08-23 19:33:54 -0300

    feat(foxy-country-widget): add select all regions

--- src/elements/public/CountryRegionWidget/CountryRegionWidget.ts
+++ src/elements/public/CountryRegionWidget/CountryRegionWidget.ts
@@ -1,3 +1,4 @@
+import { Checkbox, Dropdown, I18N } from '../../private';
 import { PropertyDeclarations, TemplateResult, html } from 'lit-element';
 import { ScopedElementsMap, ScopedElementsMixin } from '@open-wc/scoped-elements';
 import { ConfigurableMixin } from '../../../mixins/configurable';
@@ -29,59 +30,101 @@ interface Country {
 }
 
 export interface CountryValue extends Country {
-  selected_region: Region;
+  selected_regions: Array<Region>;
 }
 
 export class CountryWidget extends Base<Data> {
   static get scopedElements(): ScopedElementsMap {
     return {
-      'vaadin-combo-box': customElements.get('vaadin-combo-box'),
+      'foxy-i18n': I18N,
+      'x-checkbox': Checkbox,
+      'x-dropdown': Dropdown,
     };
   }
 
   static get properties(): PropertyDeclarations {
     return {
       ...super.properties,
+      __allRegions: { attribute: false, type: Boolean },
       __selectedCountry: { attribute: false, type: Object },
+      __selectedRegions: { attribute: false, type: Array },
     };
   }
 
   public value: CountryValue | undefined;
 
+  private __allRegions = true;
+
   private static __regionsQuery = '?include_regions';
 
   private __selectedCountry: Country | null = null;
 
-  private __selectedRegion: Region | null = null;
+  private __selectedRegions: Array<string> = [];
 
   render(): TemplateResult {
     return html`
-      <vaadin-combo-box
+      <x-dropdown
         label="country"
         data-testid="countries"
         ?disabled=${!this.data}
         @change=${this.__handleChangeEventCountry.bind(this)}
+        .getText=${(v: string) => this.t(v)}
         .items=${this.data && typeof this.data !== 'string'
           ? Object.values(this.data).map((c: any) => c.cc2)
           : []}
       >
-      </vaadin-combo-box>
+      </x-dropdown>
       ${this.__selectedCountry &&
       this.__selectedCountry.regions &&
       !Array.isArray(this.__selectedCountry.regions)
         ? html`
-            <vaadin-combo-box
-              label="region"
-              data-testid="regions"
-              .items=${Object.values(this.__selectedCountry.regions).map(r => r.c)}
-            ></vaadin-combo-box>
+            <div class="ml-m mt-s grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-s md:gap-m">
+              <x-checkbox
+                class="text-s"
+                @change=${this.__handleChangeAllRegions}
+                ?checked=${this.__allRegions}
+              >
+                <foxy-i18n lang="${this.lang}" ns="${this.ns}" key="all-regions"></foxy-i18n>
+              </x-checkbox>
+              ${this.__allRegions
+                ? ''
+                : Object.values(this.__selectedCountry.regions).map((r: Region) =>
+                    this.__renderRegionInput(r)
+                  )}
+            </div>
           `
-        : ''}
+        : html``}
     `;
   }
 
+  private __renderRegionInput(region: Region): TemplateResult {
+    if (
+      this.__selectedCountry &&
+      !Array.isArray(this.__selectedCountry.regions) &&
+      this.__selectedCountry.regions
+    ) {
+      return html`
+        <x-checkbox
+          class="text-s"
+          data-regions-input
+          data-testid="region-input-${region.c}"
+          data-region="${region.c}"
+          @change=${this.__handleChangeEventRegion}
+          ?checked=${this.__selectedRegions.includes(region.c)}
+        >
+          <foxy-i18n
+            lang="${this.lang}"
+            ns="${this.ns}"
+            key="${this.__selectedCountry.cc2}.${region.c}"
+          ></foxy-i18n>
+        </x-checkbox>
+      `;
+    } else {
+      return html``;
+    }
+  }
+
   private __hasRegions() {
-    // When a country have regions
     if (
       !this.__selectedCountry ||
       this.__selectedCountry.has_regions === false ||
@@ -99,25 +142,38 @@ export class CountryWidget extends Base<Data> {
     return (this.data as any)[cc2] as Country;
   }
 
-  private __getRegion(c: string) {
+  private __getRegion(c: string): Region | null {
     if (!this.__hasRegions()) {
       return null;
     }
-    return (this.__selectedCountry!.regions as Record<string, any>)[c];
+    return (this.__selectedCountry!.regions as Record<string, Region>)[c];
   }
 
-  private __handleChangeEventCountry(ev: Event) {
-    this.__selectedCountry = this.__getCountry((ev.target as HTMLInputElement).value);
+  private __handleChangeEventCountry(ev: CustomEvent) {
+    this.__selectedCountry = this.__getCountry(ev.detail);
+    this.__allRegions = true;
+    this.__selectedRegions = [];
   }
 
-  private __handleChangeEventRegion(ev: Event) {
-    this.__selectedRegion = this.__getRegion((ev.target as HTMLInputElement).value);
+  private __handleChangeAllRegions(ev: CustomEvent) {
+    console.debug(ev);
+    this.__allRegions = !!ev.detail;
+  }
+
+  private __handleChangeEventRegion() {
+    const regions = this.shadowRoot?.querySelectorAll('[data-regions-input]');
+    if (regions) {
+      const regionsArray = Array.from(regions) as Array<Checkbox>;
+      this.__selectedRegions = regionsArray
+        .filter((r: Checkbox) => r.checked)
+        .map((r: Checkbox) => r.getAttribute('data-region') as string);
+    }
   }
 
   private __handleChange() {
     this.value = {
       ...this.__selectedCountry!,
-      selected_region: this.__selectedRegion!,
+      selected_regions: this.__selectedRegions.map(r => this.__getRegion(r) as Region)!,
     };
     this.dispatchEvent(new Event('change'));
   }
