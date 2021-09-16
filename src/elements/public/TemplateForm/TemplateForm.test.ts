b775553e3831480d85abd9fe10315b8520c49c54
ndvo <nelson@ocastudios.com>
2021-09-16 16:40:44 -0300

    test(template-form): add tests to assess fields' visibility

--- src/elements/public/TemplateForm/TemplateForm.test.ts
+++ src/elements/public/TemplateForm/TemplateForm.test.ts
@@ -7,6 +7,72 @@ import { router } from '../../../server';
 const emailTemplateUrl = 'https://demo.foxycart.com/s/admin/email_templates/0';
 const cartTemplateUrl = 'https://demo.foxycart.com/s/admin/template/cart_templates/0';
 
+/**
+ * @param href
+ */
+function templateFormEl(href: string) {
+  return html`
+    <foxy-template-form @fetch=${(evt: FetchEvent) => router.handleEvent(evt)} href="${href}">
+    </foxy-template-form>
+  `;
+}
+
+/**
+ * @param el
+ * @param contentType
+ * @param value
+ */
+async function selectTemplateConfig(
+  el: TemplateForm,
+  contentType: 'html' | 'text' | '',
+  value: 'default' | 'url' | 'clipboard'
+) {
+  const tabs = el.shadowRoot?.querySelector('[data-testid="tabs"]');
+  if (contentType == 'html') {
+    expect(tabs).to.exist;
+    (tabs as any).value = 0;
+  } else if (contentType == 'text') {
+    expect(tabs).to.exist;
+    (tabs as any).value = 1;
+  }
+  await elementUpdated(el);
+  const choiceEl = el.shadowRoot?.querySelector(
+    `[data-testid="template-type${contentType ? '-' + contentType : ''}"]`
+  );
+  expect(choiceEl).to.exist;
+  if (choiceEl) (choiceEl as any).value = value;
+}
+
+/**
+ * @param field
+ */
+function extractType(
+  field: 'content_html_url' | 'content_text_url' | 'content_url' | 'content'
+): 'html' | 'text' | '' {
+  switch (field) {
+    case 'content_html_url':
+      return 'html';
+    case 'content_text_url':
+      return 'text';
+    case 'content_url':
+      return '';
+    case 'content':
+      return '';
+  }
+  return '';
+}
+
+it('Validates long url fields in email', async function () {
+  const el: TemplateForm = await fixture(templateFormEl(emailTemplateUrl));
+  await waitUntil(() => el.in('idle'), 'Element should become idle');
+  el.edit({ content_text_url: `http://demo.${Array(300).join('a')}.com/my_template` });
+  await elementUpdated(el);
+  const choice = el.shadowRoot?.querySelector(`[data-testid="template-type-text"]`);
+  expect(choice, `testid template-type-text does not exist.`).to.exist;
+  const error = choice?.getAttribute('error-message');
+  expect(error).to.equal('v8n_too_long');
+});
+
 describe('Input Validation', function () {
   interface inputChanges {
     changes: any;
@@ -80,10 +146,7 @@ describe('Input Validation', function () {
     it(`Validates ${contentType ? '' : 'non-'}emailTemplate ${c.field} to ${
       c.rule
     }`, async function () {
-      const el: TemplateForm = await fixture(html`
-        <foxy-template-form @fetch=${(evt: FetchEvent) => router.handleEvent(evt)} href="${c.href}">
-        </foxy-template-form>
-      `);
+      const el: TemplateForm = await fixture(templateFormEl(c.href));
       await waitUntil(() => el.in('idle'), 'Element should become idle');
       el.edit(c.changes);
       await elementUpdated(el);
@@ -96,7 +159,51 @@ describe('Input Validation', function () {
 });
 
 describe('Usability', function () {
-  it('Should hide unused elements');
+  describe('Should hide unused elements', async function () {
+    const versions = {
+      mailHtml: { fields: ['content_html_url', 'content_html'], href: emailTemplateUrl },
+      mailText: { fields: ['content_text_url', 'content_text'], href: emailTemplateUrl },
+      notMail: { fields: ['content_url', 'content'], href: cartTemplateUrl },
+    };
+    const options = {
+      clipboard: [false, true],
+      default: [false, false],
+      url: [true, false],
+    };
+    for (const [k, v] of Object.entries(versions)) {
+      for (const [option, rules] of Object.entries(options)) {
+        it(`Should hide unused fields for ${k} option ${option}`, async function () {
+          const el: TemplateForm = await fixture(templateFormEl(v.href));
+          await waitUntil(() => el.in('idle'), 'Element should become idle');
+          await selectTemplateConfig(
+            el,
+            extractType(v.fields[0] as 'content_html_url' | 'content_text_url' | 'content_url'),
+            option as keyof typeof options
+          );
+          for (let i = 0; i < 2; i++) {
+            const fieldEl: HTMLElement | undefined | null = el.shadowRoot?.querySelector(
+              `[data-testid="${v.fields[i]}"]`
+            );
+            expect(fieldEl, `data-testid=${v.fields[i]} should exist`).to.exist;
+            if (fieldEl) {
+              if (rules[i]) {
+                expect(
+                  fieldEl.offsetParent,
+                  `${v.fields[i]} should be visible when option ${option}`
+                ).to.exist;
+              } else {
+                expect(
+                  fieldEl.offsetParent,
+                  `${v.fields[i]} should be hidden when option ${option}`
+                ).to.be.null;
+              }
+            }
+          }
+        });
+      }
+    }
+  });
+
   it('Should should display overview of the possible configurations');
   it('Should should display overview of the possible configurations');
   it('Should detect it is an email template based on the href attribute');
